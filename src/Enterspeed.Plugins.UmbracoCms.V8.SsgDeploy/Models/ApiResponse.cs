905d34c760252a0d1df0cbe7ce0fe20251ae2088
Mathias Hove <mph@novicell.dk>
2022-03-11 21:17:00 +0100

    V8 SSG has been moved to V8 plugin repo

--- src/Enterspeed.Plugins.UmbracoCms.V8.SsgDeploy/Models/ApiResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Enterspeed.Plugins.UmbracoCms.V8.SsgDeploy.Models
-{
-    public class ApiResponse<T>
-    {
-        public bool IsSuccess { get; set; }
-        public string ErrorCode { get; set; }
-        public T Data { get; set; }
-    }
-}
