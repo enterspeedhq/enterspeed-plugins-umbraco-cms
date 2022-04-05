e0e524b8271ed6f910dd3f4c23e348dd1f75da43
Mathias Hove <mph@novicell.dk>
2022-04-05 12:18:13 +0200

    Changed target framework

--- Plugins/Enterspeed.Plugins.UmbracoCms.V9.SsgDeploy/src/SsgServices/DefaultServices/Netlify/SsgService.cs
+++ Plugins/Enterspeed.Plugins.UmbracoCms.V9.SsgDeploy/src/SsgServices/DefaultServices/Netlify/SsgService.cs
@@ -1,9 +1,10 @@
-﻿namespace Enterspeed.Plugins.UmbracoCms.V9.SsgDeploy.SsgServices.DefaultServices.Netlify;
-
-public class SsgService
+﻿namespace Enterspeed.Plugins.UmbracoCms.V9.SsgDeploy.SsgServices.DefaultServices.Netlify
 {
-    public virtual string Alias { get; set; }
-    public virtual string Name { get; set; }
-    public virtual string ConfigurationView { get; set; }
-    public virtual string ContentView { get; set; }
+    public class SsgService
+    {
+        public virtual string Alias { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string ConfigurationView { get; set; }
+        public virtual string ContentView { get; set; }
+    }
 }
